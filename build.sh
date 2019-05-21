1c65bd8e75652a21854ae25243ce3490ba888e7d
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-05-21 23:00:31 +0200

    Add build comments

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 cd jq-1.6
 
 # Clean previous compilation files
+echo "Clean"
 ./configure
 make distclean
+
+echo "Configure"
 
 # Configure oniguruma
 #(
@@ -16,8 +19,13 @@ make distclean
 emconfigure ./configure \
   --disable-maintainer-mode \
   --with-oniguruma=builtin || exit $?
+
+echo "Build"
 emmake make LDFLAGS=-all-static || exit $?
 
 # Generate `.wasm` file
+echo "Link"
 mv jq jq.o
 emcc jq.o -o ../jq.wasm -s ERROR_ON_UNDEFINED_SYMBOLS=0
+
+echo "Done"
